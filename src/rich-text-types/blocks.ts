a632f933cd011a36d38fef2a8718d832f7eeef42
Marvin Rudolph <info@marvin-rudolph.de>
2019-11-21 20:18:02 +0100

    :construction: Added block rich-text types

---
+++ src/rich-text-types/blocks.ts
@@ -0,0 +1,15 @@
+enum Blocks {
+  DOCUMENT = 'doc',
+  PARAGRAPH = 'paragraph',
+  HEADING = 'heading',
+  QUOTE = 'blockquote',
+  OL_LIST = 'ordered_list',
+  UL_LIST = 'bullet_list',
+  LIST_ITEM = 'list_item',
+  CODE_BLOCK = 'code_block',
+  HR = 'horizontal_rule',
+  BR = 'hard_break',
+  IMAGE = 'image'
+}
+
+export default Blocks
